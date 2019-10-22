ce21670ad53a12eb24e5ea3ba2cd35e02775ac20
Charley Chang <charleyarby@gmail.com>
2019-10-22 15:13:48 -0700

    set up basic proxy server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public/lib')));
+
+app.listen(port, ()=>{console.log(`server running at :http://localhost:${port}`)})
