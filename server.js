b2bd316401ffb6c624e17a28958ce72d9e750f2b
Charley Chang <charleyarby@gmail.com>
2019-10-22 16:39:52 -0700

    finish setting up proxy for my own review component

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.use(express.static(path.join(__dirname, 'public/lib')));
+app.use('/rooms/:id', express.static(path.join(__dirname, 'public/lib')));
 
 app.listen(port, ()=>{console.log(`server running at :http://localhost:${port}`)})
